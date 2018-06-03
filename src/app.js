757dddd966a2a1aafa8e3cc7feb1d6b0181645d9
vickyhovey <vickyhovey@gmail.com>
2018-06-03 18:51:12 -0400

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-// // import './utils.js';
-// import subtract, { square, add } from './utils.js';
+// // // import './utils.js';
+// // import subtract, { square, add } from './utils.js';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
+// // console.log('app.js is running');
+// // console.log(square(4));
+// // console.log(add(100, 23));
+// // console.log(subtract(100, 81));
 
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(21));
-console.log(isSenior(64));
+// import isSenior, { isAdult, canDrink } from './person.js';
+// console.log(isAdult(18));
+// console.log(canDrink(21));
+// console.log(isSenior(64));
 
+// install -> import -> use
+import validator from 'validator';
 
+console.log(validator.isEmail('vickyhovey@gmail.com'));
